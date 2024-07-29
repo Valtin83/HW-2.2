1d5e66597614c2e72aeb61622e62df6b8860dceb
Valentin <zhe-valentin@mail.ru>
2024-07-29 23:08:52 +0700

    WH-2.2 variables 1_3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача №1
+        int mouse = 4_766_809;
+        byte cat = 67;
+        short air = 1500;
+        long bottom = -1_678_907_059_087_000L;
+        float ship = 8.9856432f;
+        double twin = 2.43678596545098;
+        System.out.println("Значение переменной mouse типом int равно " + mouse);
+        System.out.println("Значение переменной cat типом byte равно " + cat);
+        System.out.println("Значение переменной air типом short равно " + air);
+        System.out.println("Значение переменной bottom типом long равно " + bottom);
+        System.out.println("Значение переменной ship типом float равно " + ship);
+        System.out.println("Значение переменной twin типом double равно " + twin);
+        // Задача №2
+        float a = 27.12f;
+        long b = 987_678_965_549L;
+        float d = 2.786f;
+        short c = 569;
+        short e = -159;
+        short g = 27897;
+        byte h = 67;
+        // Задача №3
+        byte studentsLP = 23;
+        byte studentsAS = 27;
+        byte studentsEA = 30;
+        int sum = studentsLP + studentsAS + studentsEA;
+        short paper = 480;
+        int paperStudent = paper / sum;
+        System.out.println("На каждого ученика рассчитано " + paperStudent + " листов бумаги.");
+
+
     }
 }
