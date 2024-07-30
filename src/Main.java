9f6f7007b5daf91174b82c3d5754950310ee984e
Valentin <zhe-valentin@mail.ru>
2024-07-30 22:43:21 +0700

    WH-2.2 variables 1_6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача №1
+//        Задача №1
         int mouse = 4_766_809;
         byte cat = 67;
         short air = 1500;
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("Значение переменной bottom типом long равно " + bottom);
         System.out.println("Значение переменной ship типом float равно " + ship);
         System.out.println("Значение переменной twin типом double равно " + twin);
-        // Задача №2
+//        Задача №2
         float a = 27.12f;
         long b = 987_678_965_549L;
         float d = 2.786f;
@@ -21,14 +21,62 @@ public class Main {
         short e = -159;
         short g = 27897;
         byte h = 67;
-        // Задача №3
+//        Задача №3
         byte studentsLP = 23;
         byte studentsAS = 27;
         byte studentsEA = 30;
-        int sum = studentsLP + studentsAS + studentsEA;
+        int sumStudent = studentsLP + studentsAS + studentsEA;
         short paper = 480;
-        int paperStudent = paper / sum;
+        int paperStudent = paper / sumStudent;
         System.out.println("На каждого ученика рассчитано " + paperStudent + " листов бумаги.");
+//        Задача №4
+        byte bottle = 16;
+        byte time = 2;
+        int oneMinuteBottle = bottle / time;
+        byte time20 = 20;
+        int efficiency20 = time20 * oneMinuteBottle;
+        byte hourMinute = 60;
+        byte hourDay = 24;
+        int minuteDay = hourDay * hourMinute;
+        int efficiencyDay = minuteDay * oneMinuteBottle;
+        byte day = 3;
+        int efficiency3Day = efficiencyDay * day;
+        byte month = 30;
+        int efficiencyMonth = efficiencyDay * month;
+        System.out.println("За " + time20 + " минут машина произвела " + efficiency20 + " штук бутылок");
+        System.out.println("За сутки машина произвела " + efficiencyDay + " штук бутылок");
+        System.out.println("За 3 дня машина произвела " + efficiency3Day + " штук бутылок");
+        System.out.println("За месяц машина произвела " + efficiencyMonth + " штук бутылок");
+//        Задача №5
+        byte paint = 120;
+        byte whitePaint = 2;
+        byte brownPaint = 4;
+        int paintClass = whitePaint + brownPaint;
+        int classes = paint / paintClass;
+        int weighingWhitePaint = classes * whitePaint;
+        int weighingBrownPaint = classes * brownPaint;
+        System.out.println("В школе, где " + classes + " классов, нужно " + weighingWhitePaint + " банок белой краски и " + weighingBrownPaint + " банок коричневой краски.");
+//         Задача №6
+        byte bananas = 5;
+        byte weight1Banana = 80;
+        int weightBananas = bananas * weight1Banana;
+        short milk = 200;
+        byte partMilk_1 = 100;
+        int partMilk = milk / partMilk_1;
+        byte weight100 = 105;
+        int weightMilk = weight100 * partMilk;
+        byte iceCream = 2;
+        byte weightIceCream_1 = 100;
+        int weightIceCream = weightIceCream_1 * iceCream;
+        byte eggs = 4;
+        byte weightEgg_1 = 70;
+        int weightEggs = eggs * weightEgg_1;
+        int weight_g = weightBananas + weightMilk + weightIceCream + weightEggs;
+        float weight_kg = (float) weight_g / 1000;
+        System.out.println(weight_g + " гр.");
+        System.out.println(weight_kg + " кг.");
+        // Задача №7
+
 
 
     }
